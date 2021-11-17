a6fe582eb1d4d9af318a63feb1b9eec4601fc21d
Nimer Amol Singh <naskbaweja@gmail.com>
2021-11-17 14:30:42 +0000

    build upload

---
+++ build/precache-manifest.11288f2c98ccdb9fcfa0fffad9c653e1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "903f1ab2a63bfd8a9f2f51469da8edaa",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "be53099d323841a357fe",
+    "url": "/home/static/css/main.aca6df45.chunk.css"
+  },
+  {
+    "revision": "f556ce46f48d6f2dfb07",
+    "url": "/home/static/js/2.b3c1645e.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.b3c1645e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be53099d323841a357fe",
+    "url": "/home/static/js/main.7bec1812.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "1780333efbf98313de5ee47e71cd160c",
+    "url": "/home/static/media/PGP.1780333e.png"
+  },
+  {
+    "revision": "0d0329e04dddf7cd8560ec33a5d95b28",
+    "url": "/home/static/media/blockchainanalysis.0d0329e0.png"
+  },
+  {
+    "revision": "ab8fc8339575d18bc8799dc20c1e5a81",
+    "url": "/home/static/media/dell.ab8fc833.png"
+  },
+  {
+    "revision": "d89c8570272da71122c1057adfc30f8f",
+    "url": "/home/static/media/neural-network.d89c8570.jpg"
+  },
+  {
+    "revision": "05c92612079581a851181ba63411b7aa",
+    "url": "/home/static/media/pirate.sardar.05c92612.jpg"
+  },
+  {
+    "revision": "6ecd3fba948a54610ecf04b2bdcbb1db",
+    "url": "/home/static/media/resume.6ecd3fba.pdf"
+  }
+]);
